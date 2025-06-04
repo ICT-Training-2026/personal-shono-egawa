304d7de5cf943a54cfba8af4a4f2d4426bd7d6c2
github Shono-Egawa <shono-egawa@kccs.co.jp>
2025-06-05 08:56:55 +0900

    初回コミット

---
+++ OBJEx04/src/ObjEx0401.java
@@ -0,0 +1,18 @@
+
+public class ObjEx0401 {
+
+	public static void main(String[] args) {
+		ObjEx040101 prod=new ObjEx040101();
+		
+		prod.name="キーボード";
+		prod.price=2000;
+		
+		prod.display();
+		prod.down(400);
+		
+		prod.display();
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
